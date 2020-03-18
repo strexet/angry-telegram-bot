4f2535c4f697756c68fc7007677e1c98932332ae
petr.strekhletov <petr.strekhletov@zimad.com>
2020-03-18 13:07:21 +0700

    Changed page content

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end('StrexetBot\n');
 });
 
 server.listen(port);
